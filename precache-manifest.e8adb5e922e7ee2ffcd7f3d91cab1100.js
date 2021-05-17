c051ce25d2ee32c6b14910c9fa881edd8a4861d4
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-05-17 18:11:37 +0200

    Updates

---
+++ precache-manifest.e8adb5e922e7ee2ffcd7f3d91cab1100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9474032879108185292211df33d2b03",
+    "url": "/ghpages/index.html"
+  },
+  {
+    "revision": "f81051097cb5c8331eae",
+    "url": "/ghpages/static/css/main.86133b36.chunk.css"
+  },
+  {
+    "revision": "1c3df010abeeff5fe4e2",
+    "url": "/ghpages/static/js/2.a59029a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages/static/js/2.a59029a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81051097cb5c8331eae",
+    "url": "/ghpages/static/js/main.5572f13b.chunk.js"
+  },
+  {
+    "revision": "014bccb55a73c7361517",
+    "url": "/ghpages/static/js/runtime-main.a1b3da30.js"
+  }
+]);
